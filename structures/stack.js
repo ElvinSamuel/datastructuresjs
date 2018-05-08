b17d62d62cec4f4e601a362d24610d4b5f29afa0
Elvin Samuel <elvinsamuel1@gmail.com>
2018-05-08 17:52:11 +0000

    Working 'linkedListTest.js' file with module.exports.

--- structures/stack.js
+++ structures/stack.js
@@ -1,4 +1,5 @@
 /* Stack */
+
 function Stack(){
     var top = null;
     var count = 0;
@@ -9,64 +10,64 @@ function Stack(){
     };
     
     /* Tacking on methods separately */
-}
-
-Stack.prototype.Push = function(data) {
-    // Creates a node containing the data and a reference to next item (if any).
-    var node = {
-        data: data,
-        next: null
+    this.Push = function(data) {
+        // Creates a node containing the data and a reference to next item (if any).
+        var node = {
+            data: data,
+            next: null
+        };
+        
+        // links current node to top.
+        node.next = top;
+        
+        // links the current node to the top node.
+        top = node;
+        
+        count++;
     };
     
-    // links current node to top.
-    node.next = top;
-    
-    // links the current node to the top node.
-    top = node;
-    
-    count++;
-};
-
-Stack.prototype.Peek = function(){
-    //If there are no items, returns null. (avoid error)
-    if(top === null){
-        return null;
-    } else {
-        return top.data;
-    }
-};
-
-Stack.prototype.Pop = function(){
-    if (top === null){
-        return null;
+    this.Peek = function(){
+        //If there are no items, returns null. (avoid error)
+        if(top === null){
+            return null;
         } else {
-        // Use the 'out' variable to save the current top.
-        var out = top;
-        top = top.next;
-        
-        if (count > 0){
-            count--;
+            return top.data;
         }
-        
-        return out.data;
-    }
-};
-
-Stack.prototype.DisplayAll = function(){
-    if(top === null){
-        return null;
-    } else {
-        //instantiate an array
-        var arr = new Array();
-        var current = top;
-        
-        // iterate through array until we reach bottom
-        for (var i = 0; i < count; i++){
-            arr[i] = current.data;;
-            current = current.next;
+    };
+    
+    this.Pop = function(){
+        if (top === null){
+            return null;
+            } else {
+            // Use the 'out' variable to save the current top.
+            var out = top;
+            top = top.next;
+            
+            if (count > 0){
+                count--;
+            }
+            
+            return out.data;
         }
-        return arr;
+    };
+    
+    this.DisplayAll = function(){
+        if(top === null){
+            return null;
+        } else {
+            //instantiate an array
+            var arr = new Array();
+            var current = top;
+            
+            // iterate through array until we reach bottom
+            for (var i = 0; i < count; i++){
+                arr[i] = current.data;;
+                current = current.next;
+            }
+            return arr;
+        }
     }
 }
+
 
 module.exports = Stack;
--- tests/linkedListTest.js
+++ tests/linkedListTest.js
@@ -2,4 +2,32 @@ var Stack = require('../structures/stack.js');
 
 var testStack = new Stack;
 
+/* TESTING TIME */
 console.log(testStack.GetCount());
+// return --> 0
+
+console.log(testStack.Push('Some String'));
+
+console.log(testStack.Push(3005));
+
+console.log(testStack.Push(true));
+
+console.log(testStack.Push(3.14));
+
+console.log(testStack.DisplayAll());
+// return --> <everything I Pushed>
+
+console.log(testStack.GetCount());
+// return --> 4
+
+console.log(testStack.Peek());
+// return --> 3.14
+
+console.log(testStack.Pop());
+// return --> 3.14
+
+console.log(testStack.Pop());
+// return --> true
+
+console.log(testStack.DisplayAll());
+// return --> <everything minus 'true' and '3.14'
