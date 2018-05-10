65a5c85bfa33717f58e467f8b1edb951e7ab4734
Elvin Samuel <elvinsamuel1@gmail.com>
2018-05-10 21:53:48 +0000

    WORKING map.js & mapTest.js files. Had to comment out the 'map2.entries()' function in map.js for now. Not sure why the compiler isn't recognizing Array.prototype.entries()

---
+++ tests/mapTest.js
@@ -0,0 +1,31 @@
+var Map = require('../structures/map.js');
+
+var map = new Map();
+
+map.set('arms', 2);
+map.set('fingers', 10);
+map.set('eyes', 2);
+map.set('belley button', 1);
+
+console.log(map.get('fingers'));
+console.log(map.size());
+console.log(map.values());
+
+var map2 = new Map();
+map2.has('hands');
+// map2.entries();
+
+let keyObj = {},
+    keyFunc = function(){};
+    
+map2.set('hello', 'string value');
+map2.set(keyObj, 'obj value');
+map2.set(keyFunc, 'func value');
+map2.set(NaN, 'NaN value');
+
+console.log(map2.size);
+
+console.log(map2.get('hello'));
+console.log(map2.get(keyObj));
+console.log(map2.get(keyFunc));
+console.log(map2.get(NaN));
